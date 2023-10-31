729e944917bab2a325a75abc62525d41f434e760
Vit2979 <86978620+Vit2979@users.noreply.github.com>
2023-10-31 15:57:50 +0300

    fix error loading

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,26 +1,31 @@
 import React, { useEffect } from 'react';
+
 interface SearchInputProps {
-searchQuery: string;
-onSearchQueryChange: (query: string) => void;
-onSearchClick: () => void;
+  searchQuery: string;
+  onSearchQueryChange: (query: string) => void;
+  onSearchClick: () => void;
 }
+
 function SearchInput(props: SearchInputProps) {
-const { searchQuery, onSearchQueryChange, onSearchClick } = props;
-useEffect(() => {
-onSearchClick();
-}, []);
-return (
-<div id="top-section">
-<input
-type="text"
-value={searchQuery}
-onClick={() => onSearchQueryChange('')}
-onChange={(e) => onSearchQueryChange(e.target.value)}
-/>
-<button type="button" onClick={onSearchClick}>
-Search
-</button>
-</div>
-);
+  const { searchQuery, onSearchQueryChange, onSearchClick } = props;
+
+  useEffect(() => {
+    onSearchClick();
+  }, [onSearchClick]);
+
+  return (
+    <div id="top-section">
+      <input
+        type="text"
+        value={searchQuery}
+        onClick={() => onSearchQueryChange('')}
+        onChange={(e) => onSearchQueryChange(e.target.value)}
+      />
+      <button type="button" onClick={onSearchClick}>
+        Search
+      </button>
+    </div>
+  );
 }
+
 export default SearchInput;
